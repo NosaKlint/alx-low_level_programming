e778a09e15607af66e6bc46b574ebcc4641b53cd
NosaKlint <nosa.omorodion@outlook.com>
2022-06-06 12:39:37 -0700

    ds

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-ptints the aphabet in lowercase
+ *
+ */
+int main(void)
+  char letter;
+  for putchar('\n');
+  return (0);
+  }
+
+          putchar(letter);
+
+
