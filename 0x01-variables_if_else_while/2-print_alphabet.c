bfaf8004c55cb9065355c7f893224f0a9840101d
NosaKlint <nosa.omorodion@outlook.com>
2022-06-06 16:37:58 -0700

    new

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 /**
- * main-ptints the aphabet in lowercase
+ * main-Prints the alphabet in lowercase.
  *
+ * Return: Always 0.
  */
 int main(void)
-  char letter;
-  for putchar('\n');
-  return (0);
-  }
+{
+	char letter;
 
-          putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
+	putchar{'\n');
+		return (0);
+	}
 
